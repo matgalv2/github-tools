3a23d2b41f1f79ed2304d7cad2b0f94d009db329
Mateusz <matgalv2@gmail.com>
2024-02-11 11:22:26 +0100

    Initial commit

---
+++ src/main/java/io/github/matgalv2/githubtools/GithubToolsApplication.java
@@ -0,0 +1,13 @@
+package io.github.matgalv2.githubtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/matgalv2/githubtools/GithubToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matgalv2.githubtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
