60d22e7737d695cbe184bedeca11380ac288ff05
Mateusz <matgalv2@gmail.com>
2024-02-15 13:00:54 +0100

    Moved converter to separate file

--- src/main/java/io/github/matgalv2/githubtools/controller/GithubExplorerController.java
+++ src/main/java/io/github/matgalv2/githubtools/controller/GithubExplorerController.java
@@ -1,15 +1,9 @@
 package io.github.matgalv2.githubtools.controller;
 
 
-import io.github.matgalv2.githubtools.dto.BranchDTO;
-import io.github.matgalv2.githubtools.dto.RepositoryDTO;
-import io.github.matgalv2.githubtools.githubapi.Branch;
-import io.github.matgalv2.githubtools.githubapi.Repository;
+import io.github.matgalv2.githubtools.converter.RepositoryConverter;
 import io.github.matgalv2.githubtools.service.GithubExplorerService;
 import lombok.AllArgsConstructor;
-import org.modelmapper.Converter;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -22,37 +16,17 @@ import javax.validation.constraints.NotBlank;
 public class GithubExplorerController {
 
     private final GithubExplorerService githubExplorerService;
-    private final ModelMapper mapper;
+    private final RepositoryConverter converter;
 
     @GetMapping("/repos/{username}")
     public ResponseEntity<?> getUserRepositories(@PathVariable @NotBlank String username){
         return githubExplorerService
                 .getUserRepositories(username)
-                .map(repositories -> repositories.stream().map(repositoryToAPI()::map))
+                .map(repositories -> repositories.stream().map(converter.repositoryToAPI()::map))
                 .fold(
                         error -> ResponseEntity.status(error.getStatus()).body(error),
                         ResponseEntity::ok
                 );
-
-    }
-
-    private TypeMap<Repository, RepositoryDTO> repositoryToAPI() {
-        Converter<Repository, RepositoryDTO> converter = mappingContext -> {
-            RepositoryDTO destination = mappingContext.getDestination();
-            destination.setBranches(mappingContext.getSource().getBranches().stream().map(branchToAPI()::map).toList());
-            return destination;
-        };
-
-        return mapper
-                .typeMap(Repository.class, RepositoryDTO.class)
-                .addMapping(Repository::getName, RepositoryDTO::setRepositoryName)
-                .setPostConverter(converter);
-    }
-
-    private TypeMap<Branch, BranchDTO> branchToAPI() {
-        return mapper
-                .typeMap(Branch.class, BranchDTO.class)
-                .addMapping(Branch::getLastCommitSha, BranchDTO::setLastCommit);
     }
 
 
