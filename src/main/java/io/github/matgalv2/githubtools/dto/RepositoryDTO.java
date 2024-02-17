f8eb10b29613dded9bc36e8e654d9b2af3afb89b
Mateusz <matgalv2@gmail.com>
2024-02-18 00:17:24 +0100

    Fixed style

--- src/main/java/io/github/matgalv2/githubtools/dto/RepositoryDTO.java
+++ src/main/java/io/github/matgalv2/githubtools/dto/RepositoryDTO.java
@@ -2,14 +2,16 @@ package io.github.matgalv2.githubtools.dto;
 
 
 import io.github.matgalv2.githubtools.common.Error;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public  class RepositoryDTO {
+public class RepositoryDTO {
     private String ownerLogin;
     private String repositoryName;
     private List<BranchDTO> branches;
