60af98a058ea64387009949f3e4ef3e37730288d
mateusz <matgalv2@gmail.com>
2024-07-18 13:10:08 +0200

    Updated

--- src/main/java/io/github/matgalv2/githubtools/GithubToolsApplication.java
+++ src/main/java/io/github/matgalv2/githubtools/GithubToolsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubToolsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GithubToolsApplication.class, args);
     }
