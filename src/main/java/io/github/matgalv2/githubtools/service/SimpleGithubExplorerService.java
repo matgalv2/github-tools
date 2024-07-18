5284f433e20ff45c08559cc0a5273c14d760f088
mateusz <matgalv2@gmail.com>
2024-07-18 13:09:55 +0200

    Injecting properties Changed String#format to StringTemplate

--- src/main/java/io/github/matgalv2/githubtools/service/SimpleGithubExplorerService.java
+++ src/main/java/io/github/matgalv2/githubtools/service/SimpleGithubExplorerService.java
@@ -6,6 +6,7 @@ import io.github.matgalv2.githubtools.error.ServiceException;
 import io.github.matgalv2.githubtools.githubapi.Branch;
 import io.github.matgalv2.githubtools.githubapi.Repository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -13,8 +14,8 @@ import org.springframework.web.client.RestClient;
 import java.net.URI;
 import java.util.List;
 
+import static java.lang.StringTemplate.STR;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 
 @Service
 @RequiredArgsConstructor
@@ -22,15 +23,17 @@ public class SimpleGithubExplorerService implements GithubExplorerService {
 
     private final RestClient client;
 
-    private static final String reposURL = "https://api.github.com/users/%s/repos";
-    private static final String branchesURL = "https://api.github.com/repos/%s/%s/branches";
+    @Value("${github-repositoriesURL}")
+    private String repositoriesURL;
 
+    @Value("${github-branchesURL}")
+    private String branchesURL;
 
     @Override
     public List<Repository> getUserRepositories(String username) {
         ParameterizedTypeReference<List<Repository>> typeRef = new ParameterizedTypeReference<>() {};
 
-        List<Repository> response = getRequest(String.format(reposURL, username), typeRef);
+        List<Repository> response = getRequest(String.format(repositoriesURL, username), typeRef);
         return response
                 .stream().filter(repository -> !repository.isFork())
                 .map(repository -> {
@@ -50,7 +53,7 @@ public class SimpleGithubExplorerService implements GithubExplorerService {
             return getRequest(branchesURL, typeRef);
         }
         catch (ServiceException exception){
-            String message = String.format("Could not get branches for repository %s due to error: %s", repositoryName, exception.getError().getMessage());
+            String message = STR."Could not get branches for repository \{repositoryName} due to error: \{exception.getError().getMessage()}";
             Error error = new Error(exception.getError().getStatus(), message);
             throw new ServiceException(error);
         }
@@ -64,7 +67,7 @@ public class SimpleGithubExplorerService implements GithubExplorerService {
                     .get()
                     .uri(URI.create(url))
                     .accept(APPLICATION_JSON)
-                    .exchange((req, resp) -> {
+                    .exchange((_, resp) -> {
                         if (resp.getStatusCode().is4xxClientError())
                             throw new ServiceException(new Error(resp.getStatusCode().value(), resp.getStatusText()));
                         else
@@ -73,7 +76,7 @@ public class SimpleGithubExplorerService implements GithubExplorerService {
         } catch (ServiceException error) {
             throw new ServiceException(error.getError());
         } catch (Exception exception) {
-            String message = String.format("Could not connect to %s", url);
+            String message = STR."Could not connect to \{url}";
             throw new CouldNotConnectToGithubApi(message);
         }
         return response;
