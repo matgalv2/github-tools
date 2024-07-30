065de0a422a5ba2247cfc1d3de95bc3038de7cbe
mateusz <matgalv2@gmail.com>
2024-07-30 13:54:13 +0200

    Changed error code to 503

--- src/main/java/io/github/matgalv2/githubtools/error/CouldNotConnectToGithubApi.java
+++ src/main/java/io/github/matgalv2/githubtools/error/CouldNotConnectToGithubApi.java
@@ -3,6 +3,6 @@ package io.github.matgalv2.githubtools.error;
 public class CouldNotConnectToGithubApi extends ApplicationException {
 
     public CouldNotConnectToGithubApi(String message) {
-        super(new Error(500, message));
+        super(new Error(503, message));
     }
 }
