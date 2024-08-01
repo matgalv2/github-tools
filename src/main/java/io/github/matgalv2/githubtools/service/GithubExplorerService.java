1b315733fb8fec80d2f713d43a5e744587074548
mateusz <matgalv2@gmail.com>
2024-08-01 16:39:06 +0200

    Moved mapping to service

--- src/main/java/io/github/matgalv2/githubtools/service/GithubExplorerService.java
+++ src/main/java/io/github/matgalv2/githubtools/service/GithubExplorerService.java
@@ -1,12 +1,10 @@
 package io.github.matgalv2.githubtools.service;
 
 
-import io.github.matgalv2.githubtools.error.Error;
-import io.github.matgalv2.githubtools.githubapi.Repository;
-import io.vavr.control.Either;
+import io.github.matgalv2.githubtools.dto.RepositoryDTO;
 
 import java.util.List;
 
 public interface GithubExplorerService {
-    List<Repository> getUserRepositories(String username);
+    List<RepositoryDTO> getUserRepositories(String username);
 }
