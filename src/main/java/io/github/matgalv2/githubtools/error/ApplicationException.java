5349b3feb508c4175532eacf64690d95a1a68003
mateusz <matgalv2@gmail.com>
2024-08-01 16:38:46 +0200

    Refactored exceptions

--- src/main/java/io/github/matgalv2/githubtools/error/ApplicationException.java
+++ src/main/java/io/github/matgalv2/githubtools/error/ApplicationException.java
@@ -3,10 +3,10 @@ package io.github.matgalv2.githubtools.error;
 import lombok.Getter;
 
 @Getter
-public abstract class ApplicationException extends RuntimeException {
+public class ApplicationException extends RuntimeException {
     private final Error error;
 
-    protected ApplicationException(Error error) {
+    public ApplicationException(Error error) {
         super(error.getMessage());
         this.error = error;
     }
--- src/main/java/io/github/matgalv2/githubtools/error/CouldNotConnectToGithubApi.java
+++
@@ -1,8 +0,0 @@
-package io.github.matgalv2.githubtools.error;
-
-public class CouldNotConnectToGithubApi extends ApplicationException {
-
-    public CouldNotConnectToGithubApi(String message) {
-        super(new Error(503, message));
-    }
-}
