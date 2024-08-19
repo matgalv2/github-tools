b664ac698c887caeb7bdc875c418a341c3bd64c9
mateusz <matgalv2@gmail.com>
2024-08-19 11:49:32 +0200

    Revert "Updated tests"
    This reverts commit 6a24fc7c

--- src/test/java/io/github/matgalv2/githubtools/controller/GithubExplorerControllerIntegrationTest.java
+++ src/test/java/io/github/matgalv2/githubtools/controller/GithubExplorerControllerIntegrationTest.java
@@ -3,26 +3,25 @@ package io.github.matgalv2.githubtools.controller;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import io.github.matgalv2.githubtools.dto.BranchDTO;
-import io.github.matgalv2.githubtools.dto.RepositoryDTO;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
-
-import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.CoreMatchers.is;
 
 
 @AutoConfigureMockMvc
@@ -30,8 +29,8 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GithubExplorerControllerIntegrationTest {
 
-
-    private WebTestClient webTestClient;
+	@Autowired
+    private MockMvc mockMvc;
     @Value("${github.api.endpoint.repositories}")
     private String endpointRepositories;
 
@@ -46,16 +45,11 @@ public class GithubExplorerControllerIntegrationTest {
 		registry.add("github.api.baseURL", wireMockExtension::baseUrl);
 	}
 
-	@BeforeEach
-	public void setUp(ApplicationContext applicationContext) {
-		webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build();
-	}
-
     @Test
-    public void test_existing_user_correct_response(){
+    public void test_existing_user_correct_response() throws Exception {
         String username = "username";
 
-        wireMockExtension.stubFor(WireMock.get(urlMatching(endpointRepositories.replaceFirst("\\{username}", username)))
+        wireMockExtension.stubFor(WireMock.get(urlMatching(String.format(endpointRepositories, username)))
                 .willReturn(
                         aResponse()
                                 .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
@@ -63,36 +57,24 @@ public class GithubExplorerControllerIntegrationTest {
 						[
 						    {
 							    "name": "repo1",
-							    "owner": {
-							    	"login": "username"
-							    },
 							    "branches_url": "https://api.github.com/repos/user/repo1/branches{/branch}",
 							    "fork": false,
 							    "private": false
 						    },
 						    {
 						        "name": "repo2",
-							    "owner": {
-							    	"login": "username"
-							    },
 							    "branches_url": "https://api.github.com/repos/user/repo2/branches{/branch}",
 							    "fork": true,
 							    "private": false
 						    },
 						    {
 							    "name": "repo3",
-							    "owner": {
-							    	"login": "username"
-							    },
 							    "branches_url": "https://api.github.com/repos/user/repo3/branches{/branch}",
 							    "fork": false,
 							    "private": true
 						    },
 						    {
 						        "name": "repo4",
-							    "owner": {
-							    	"login": "username"
-							    },
 							    "branches_url": "https://api.github.com/repos/user/repo4/branches{/branch}",
 							    "fork": true,
 							    "private": true
@@ -100,7 +82,7 @@ public class GithubExplorerControllerIntegrationTest {
 						]
 						""")));
 
-		wireMockExtension.stubFor(WireMock.get(urlMatching(endpointBranches.replaceFirst("\\{username}", ".*").replaceFirst("\\{repository}" , ".*")))
+		wireMockExtension.stubFor(WireMock.get(urlMatching(String.format(endpointBranches, ".*", ".*")))
 				.willReturn(
 						aResponse()
 								.withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
@@ -123,28 +105,22 @@ public class GithubExplorerControllerIntegrationTest {
 						]
 						""")));
 
-		RepositoryDTO expected = new RepositoryDTO(
-				username,
-				"repo1",
-				List.of(
-						new BranchDTO("master", "ef32ff17ea4b7eb7936db1158470fa3c257864c3"),
-						new BranchDTO("main", "caa40305d00b18d4bc2669fcd63fb5c2e075136b")
-				)
-		);
-        webTestClient
-				.get()
-				.uri("/repos/{username}", username)
-				.exchange()
-				.expectStatus().isOk()
-				.expectBodyList(RepositoryDTO.class)
-				.isEqualTo(List.of(expected));
+        this.mockMvc
+				.perform(get("/repos/{username}", username))
+				.andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].ownerLogin", is(username)))
+                .andExpect(jsonPath("$.[0].repositoryName", is("repo1")))
+                .andExpect(jsonPath("$.[0].branches.[0].name", is("master")))
+                .andExpect(jsonPath("$.[0].branches.[0].lastCommit", is("ef32ff17ea4b7eb7936db1158470fa3c257864c3")))
+                .andExpect(jsonPath("$.[0].branches.[1].name", is("main")))
+                .andExpect(jsonPath("$.[0].branches.[1].lastCommit", is("caa40305d00b18d4bc2669fcd63fb5c2e075136b")));
 
     }
 
 	@Test
 	public void test_nonexistent_user_not_found_response() throws Exception {
-		String nonExistentUsername = "nonExistentUsername";
-		wireMockExtension.stubFor(WireMock.get(urlMatching(endpointRepositories.replaceFirst("\\{username}", "username")))
+		String username = "username";
+		wireMockExtension.stubFor(WireMock.get(urlMatching(String.format(endpointRepositories, username)))
 				.willReturn(
 						aResponse()
 								.withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
@@ -152,9 +128,6 @@ public class GithubExplorerControllerIntegrationTest {
 						[
 						    {
 							    "name": "repo1",
-							    "owner": {
-							    	"login": "username"
-							    },
 							    "branches_url": "https://api.github.com/repos/user/repo1/branches{/branch}",
 							    "fork": false,
 							    "private": false
@@ -162,7 +135,7 @@ public class GithubExplorerControllerIntegrationTest {
 						]
 						""")));
 
-		wireMockExtension.stubFor(WireMock.get(urlMatching(endpointBranches.replaceFirst("\\{username}", ".*").replaceFirst("\\{repository}" , ".*")))
+		wireMockExtension.stubFor(WireMock.get(urlMatching(String.format(endpointBranches, ".*", ".*")))
 				.willReturn(
 						aResponse()
 								.withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
@@ -178,24 +151,20 @@ public class GithubExplorerControllerIntegrationTest {
 						]
 						""")));
 
-		webTestClient
-				.get()
-				.uri("/repos/{username}", nonExistentUsername)
-				.exchange()
-				.expectStatus().isNotFound();
+		this.mockMvc
+				.perform(get("/repos/{username}", "nonexistent_user"))
+				.andExpect(status().isNotFound());
 	}
 
 	@Test
 	public void test_githubApiError_internal_server_error_response() throws Exception {
 		String username = "username";
-		wireMockExtension.stubFor(WireMock.get(urlMatching(endpointRepositories.replaceFirst("\\{username}", username)))
+		wireMockExtension.stubFor(WireMock.get(urlMatching(String.format(endpointRepositories, username)))
 				.willReturn(serverError()));
 
-		webTestClient
-				.get()
-				.uri("/repos/{username}", username)
-				.exchange()
-				.expectStatus().is5xxServerError();
+		this.mockMvc
+				.perform(get("/repos/{username}", username))
+				.andExpect(status().isInternalServerError());
 	}
 
 }
